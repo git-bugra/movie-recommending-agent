b61177a8c1508ac7717c67fd5280a53697dc4347
bugra-git <bugraozer791@gmail.com>
2025-12-31 17:24:57 +0000

    Structural fixes and code improvement
                -Fix persistent condition affect,
                -Add future recovery tools,
                -Add intuitive columns,
                -Add internally limited columns,
                -Fix indentation errors,
                -Add increased readability,
                -Add more conventional verb-noun pairs.

--- movie-picking-agent.py
+++ movie-picking-agent.py
@@ -11,17 +11,25 @@ request=requests.get(f'http://www.omdbapi.com/?apikey={key}&i=tt0108052')
 class MovieAgent():
     '''Main object. Carries data internally'''
     def __init__(self):
-        self.injectData()
+        self._setupAgent
         self.data=None
+        self.raw_data=None
         self.condition=None
 
-    def injectData(self):
-        '''Inject imdb data to program'''
+    def _setupAgent(self):
+        '''Orchestrates the flow of code for easy readability.'''
+        self.setupData()
+        self.applyRenamedColumns() #Rename the columns
+        self.assignCondition('IMDBid', 'Average Rating', 'Number of Votes', 'Primary Title', 'Published', '')
+
+    def setupData(self):
+        '''Setup imdb data to program and call on files to be merged'''
         title_imr=pl.Path(__file__).parent / 'data' / 'imdb.title.ratings.tsv' #imr=id, metadata, rating
         title_basics=pl.Path(__file__).parent / 'data' / 'imdb.title.basics.tsv' #
         title_basics_df=self.assignPath(title_basics)
         title_imr_df=self.assignPath(title_imr)
         self.mergeDataFrames(title_imr_df,title_basics_df) #insert df to be merged
+        #Space to insert pretty print
         return self
 
     def assignPath(self, path: str):
@@ -39,34 +47,55 @@ class MovieAgent():
         if len(args)>1:
             for i in range(1,len(args)):
                 result=result.merge(args[i],on='tconst')
+        self.raw_data=result
         self.data=result
         return self
     
+    def applyRenamedColumns(self):
+            '''Make columns more readable and intuitive'''
+            self.data=self.data.rename(columns={'tconst': 'IMDBid', 
+                                      'averageRating': 'Average Rating', 
+                                      'numVotes': 'Number of Votes', 
+                                      'titleType': 'Title Type', 
+                                      'primaryTitle': 'Primary Title',
+                                      'originalTitle': 'Original Title',
+                                      'isAdult': 'Is Adult',
+                                      'startYear': 'Published',
+                                      'endYear': 'End Year',
+                                      'runtimeMinutes': 'Run Time Minutes',
+                                      'genres': 'Genre'})
+            
+            print(self.data.columns)
+            return self
+
     def assignCondition(self, *args:str):
-        '''Limit the data with given columns.
+        '''Internal limitation the data with given columns.
+        Mutates data to given arguments.
         
         *args: Names of the columns to limit'''
         columns_to_limit=[*args]
         if len(columns_to_limit)>1:self.condition=columns_to_limit
-        configureFile(self)
+        self.applyCondition()
         return self
 
-def configureFile(movie_agent:MovieAgent):
-    '''Based on condition, adjust the data to display'''
-    if movie_agent and movie_agent.condition:
-        movie_agent.data=movie_agent.data[movie_agent.condition]
-    elif not movie_agent:
-        raise ValueError(f'Failed to configure the file. MovieAgent.data: {movie_agent.data}')
-    return movie_agent
+    def applyCondition(self):
+        '''Based on condition, adjust the data to display'''
+        if not self:
+            raise ValueError(f'Failed to apply condition to the file.')
+        if self and self.condition:
+            self.data=self.data[self.condition]
+            self.condition=False
+        return self
+    
+    def filterData():
+            ''''''
 
-def configureAPI():
+def initAPI():
     '''Pending'''
 
-def configureCLI():
-    '''Manipulate and communicate to the object and take actions via CLI commands.'''
-
-def genreFiltering():
-    ''''''
+def initCLI():
+    '''Manipulate and communicate to the object and take actions via CLI commands.
+    -WIP'''
 
 def recommendationLogic():
     '''
@@ -75,20 +104,22 @@ def recommendationLogic():
 if __name__ == '__main__':
     main=MovieAgent()
 
-    '''Initialize main callables and set data
+    '''Structural fixes and code improvement
             
-            -Add internal table configuration,
-            -Fix verbose and unecessary variables,
+            -Fix persistent condition affect, 
+            -Add future recovery tools,
+            -Add intuitive columns,
+            -Add internally limited columns,
+            -Fix indentation errors,
             -
             
     TODO:   
             -Make program less concerete (imdb data needs downloaded somehow)
             -Sort the loaded movies with top ratings,
             -Recommend the top n amount,
-            -Put previously loaded to memory,
+            -Add previously loaded to memory,
             -Load random top n amount, excluding previously loaded,
-
-
+            -Add user filtering.
             
     Current:
     
