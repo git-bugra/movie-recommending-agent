de6a3ef3a521dacc9d4dcdc267e5430cb6bf3bec
bugra-git <bugraozer791@gmail.com>
2026-01-01 13:49:24 +0000

    Major code improvements and refactoring.
                -Fix incomplete function setups,
                -Add error handling,
                -Add intuitive descriptions to lines of code,
                -Add internal boolean indexing,
                -Add refactored version of SRP violating class,
                -Add internal recovery tools,
                -Fix/reduce excessively data mutating helper methods.

--- movie-picking-agent.py
+++ movie-picking-agent.py
@@ -8,65 +8,30 @@ import pathlib as pl
 request=requests.get(f'http://www.omdbapi.com/?apikey={key}&i=tt0108052')
 '''
 
-class MovieAgent():
+class movieAgent():
     '''Main object. Carries data internally'''
     def __init__(self):
-        self._setupAgent
         self.data=None
-        self.raw_data=None
         self.condition=None
-
-    def _setupAgent(self):
-        '''Orchestrates the flow of code for easy readability.'''
-        self.setupData()
-        self.applyRenamedColumns() #Rename the columns
-        self.assignCondition('IMDBid', 'Average Rating', 'Number of Votes', 'Primary Title', 'Published', '')
-
-    def setupData(self):
-        '''Setup imdb data to program and call on files to be merged'''
-        title_imr=pl.Path(__file__).parent / 'data' / 'imdb.title.ratings.tsv' #imr=id, metadata, rating
-        title_basics=pl.Path(__file__).parent / 'data' / 'imdb.title.basics.tsv' #
-        title_basics_df=self.assignPath(title_basics)
-        title_imr_df=self.assignPath(title_imr)
-        self.mergeDataFrames(title_imr_df,title_basics_df) #insert df to be merged
-        #Space to insert pretty print
-        return self
-
-    def assignPath(self, path: str):
-        '''Assign path to user selected dir'''
-        path = pl.Path(path)
-        try:
-            main = pd.read_csv(path, delimiter='\t') #Read file
-        except Exception as e:
-            raise IOError(f"Failed to read CSV: {e}") from e
-        return main
-    
-    def mergeDataFrames(self,*args:pd.DataFrame):
-        '''Merge .tsv data files'''
-        result=args[0]
-        if len(args)>1:
-            for i in range(1,len(args)):
-                result=result.merge(args[i],on='tconst')
-        self.raw_data=result
-        self.data=result
-        return self
     
     def applyRenamedColumns(self):
-            '''Make columns more readable and intuitive'''
-            self.data=self.data.rename(columns={'tconst': 'IMDBid', 
-                                      'averageRating': 'Average Rating', 
-                                      'numVotes': 'Number of Votes', 
-                                      'titleType': 'Title Type', 
-                                      'primaryTitle': 'Primary Title',
-                                      'originalTitle': 'Original Title',
-                                      'isAdult': 'Is Adult',
-                                      'startYear': 'Published',
-                                      'endYear': 'End Year',
-                                      'runtimeMinutes': 'Run Time Minutes',
-                                      'genres': 'Genre'})
-            
-            print(self.data.columns)
+        '''Make columns in imdb .tsv files more readable and intuitive'''
+        try:    
+            self.data:pd.DataFrame=self.data.rename(columns={'tconst': 'IMDBid', 
+                                    'averageRating': 'Average Rating', 
+                                    'numVotes': 'Number of Votes', 
+                                    'titleType': 'Title Type', 
+                                    'primaryTitle': 'Primary Title',
+                                    'originalTitle': 'Original Title',
+                                    'isAdult': 'Is Adult',
+                                    'startYear': 'Published',
+                                    'endYear': 'End Year',
+                                    'runtimeMinutes': 'Run Time Minutes',
+                                    'genres': 'Genre'})
             return self
+
+        except: KeyError('Column not found to rename.')
+        
 
     def assignCondition(self, *args:str):
         '''Internal limitation the data with given columns.
@@ -75,7 +40,8 @@ class MovieAgent():
         *args: Names of the columns to limit'''
         columns_to_limit=[*args]
         if len(columns_to_limit)>1:self.condition=columns_to_limit
-        self.applyCondition()
+        try:self.applyCondition()
+        except:KeyError('Column given to limit not found')
         return self
 
     def applyCondition(self):
@@ -84,11 +50,61 @@ class MovieAgent():
             raise ValueError(f'Failed to apply condition to the file.')
         if self and self.condition:
             self.data=self.data[self.condition]
-            self.condition=False
+            self.condition=False #Consume condition after applying for predictable code
+            print(self.data['Primary Title'], '\n')
         return self
     
-    def filterData():
-            ''''''
+    def applyInternalFilter(self, mask:str):
+        '''Remove unwanted records internally (mostly rows that are not movie )'''
+        self.data=self.data[self.data['titleType']==mask]
+
+class movieAgentBuilder():
+    '''Orchestral class for managing the conceptual level and managing callable hierarchy.'''
+
+    def __init__(self):
+        self._initAgent()
+        self.raw_data=None
+
+    def _initAgent(self):
+        '''Orchestrates the flow of code for easy readability.'''
+        movie_agent=movieAgent()
+        self.setupData(movie_agent)
+        movie_agent.applyInternalFilter('movie') #remove anything else than movie in records
+        movie_agent.applyRenamedColumns() #Rename the columns to be more intuitive
+        movie_agent.assignCondition('IMDBid', 'Average Rating', 'Number of Votes', 'Primary Title', 'Published', 'Genre')
+
+    def setupData(self, movie_agent:movieAgent):
+        '''Setup imdb data and call on files to be merged'''
+        title_imr=pl.Path(__file__).parent / 'data' / 'imdb.title.ratings.tsv' #imr=id, metadata, rating
+        title_basics=pl.Path(__file__).parent / 'data' / 'imdb.title.basics.tsv' #
+        title_basics_df=self.assignPath(title_basics)
+        title_imr_df=self.assignPath(title_imr)
+        movie_agent.data=self.mergeDataFrames(title_imr_df,title_basics_df) #insert df to be merged
+        self.raw_data=self.setupRawData(movie_agent)
+        return movie_agent
+    
+    def mergeDataFrames(self,*args:pd.DataFrame):
+        '''Merges .tsv data files. Mutates self.data.'''
+        result=args[0]
+        if len(args)>1:
+            for i in range(1,len(args)):
+                result=result.merge(args[i],on='tconst')
+        return result
+
+    def assignPath(self, path: str):
+        '''Assign path to user selected dir'''
+        path=pl.Path(path)
+        try:
+            readTSV=pd.read_csv(path, delimiter='\t') #Read file
+        except Exception as e:
+            raise IOError(f"Failed to read CSV: {e}") from e
+        return readTSV
+
+    def setupRawData(self, movie_agent:movieAgent):
+        '''Copies the raw data of movieAgent object's df'''
+        self.raw_data=movie_agent.data
+        return self.raw_data
+
 
 def initAPI():
     '''Pending'''
@@ -102,16 +118,15 @@ def recommendationLogic():
     '''
 
 if __name__ == '__main__':
-    main=MovieAgent()
-
+    builder=movieAgentBuilder()
     '''Structural fixes and code improvement
             
-            -Fix persistent condition affect, 
-            -Add future recovery tools,
-            -Add intuitive columns,
-            -Add internally limited columns,
-            -Fix indentation errors,
-            -
+            -Fix incomplete function setups,
+            -Add error handling, 
+            -Add intuitive descriptions to lines of code,
+            -Add internal boolean indexing,
+            -Add refactored version of SRP violating class,
+            -Add internal recovery tools,
             
     TODO:   
             -Make program less concerete (imdb data needs downloaded somehow)
@@ -125,7 +140,8 @@ if __name__ == '__main__':
     
             -Load data files,
             -Read .tsv files as pandas df object,
-            -Call on condition to limit the view of the df.'''
+            -Call on condition to limit the view of the df,
+            -Manipulate columns on the df'''
 
     
 
